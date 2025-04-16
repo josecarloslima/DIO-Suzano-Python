df445b0da6ddaf5ee61d8f7275d99e59f8022a66
josecarloslima <josecarloslima@gmail.com>
2025-04-16 20:20:17 -0300

    adição do ola_mundo_pkg

---
+++ ola_mundo_pkg/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="ola_mundo",
+    version="0.0.1",
+    author="jose_carlos_lima",
+    description="Uma saudação para te livrar da maldição!",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="my_github_repository_project_link",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',    
+    )
